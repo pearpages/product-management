77aded655db9f68f8d3c515ef7b4d3b19d10b1f3
pearpages <pere@soms.cat>
2016-04-27 11:30:42 +0200

    Routing by code

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,17 +1,31 @@
-import {Component} from 'angular2/core';
+import {Component,OnInit} from 'angular2/core';
 import {RouteParams,Router} from 'angular2/router';
+import {ProductHttpService} from './product-http.service';
+import {IProduct} from './product';
 
 @Component({
     templateUrl: 'app/products/product-detail.component.html'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit{
     pageTitle: string = 'Product Detail';
-    constructor(private _routeParams: RouteParams,private _router: Router) {
+    product: IProduct;
+    errorMessage: string;
+    constructor(private _routeParams: RouteParams,private _router: Router,private _productService: ProductHttpService) {
         let id = +this._routeParams.get('id');
         this.pageTitle += `: ${id}`;
     }
     
     onBack(): void {
         this._router.navigate(['Products']);
+    }
+    
+    ngOnInit():void {
+        let all;
+        
+         this._productService.getProducts()
+        .subscribe(
+            products => this.product = products,
+            error => this.errorMessage = <any>error
+        );
     }
 }
